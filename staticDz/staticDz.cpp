5ea3474b37d65ba47ad39d9a417d5760c4c3fab1
kakishkak <kolya.yokimavich@gmail.com>
2024-12-08 19:48:44 +0200

    Update staticDz.cpp

--- staticDz/staticDz.cpp
+++ staticDz/staticDz.cpp
@@ -6,24 +6,24 @@ using namespace std;
 class Student {
 	string name;
 public:
-	Student() {
-		name = "Lolik";
-	}
-	Student(string x) {
-		this->name = x;
-	}
-	void AssingValues(string x) {
-		this->name = x;
-	}
-	void Printer() {
-		cout << "Name: " << name << endl;
-	}
-	string getName() {
-		return this->name;
-	}
-	void setMark(string x) {
-		this->name = x;
-	}
+	Student() {		   //
+		name = "Lolik";//
+	}				   //Little cute constructors
+	Student(string x) {//
+		this->name = x;//
+	}				   //
+	void AssingValues(string x) {//
+		this->name = x;			 //Assign
+	}							 //
+	void Printer() {					 //
+		cout << "Name: " << name << endl;//Printer
+	}									 //
+	string getName() {	  //
+		return this->name;//Get
+	}					  //
+	void setMark(string x) {//
+		this->name = x;		//Set
+	}						//
 };
 
 class Group {
@@ -33,33 +33,33 @@ public:
 	Group() {
 		slaves = new Student[amountOfSlaves];
 	}
-	void Adder(Student slave) {
-		IncrementIncrease(1);
-		Student* temporaryArr = new Student[amountOfSlaves];
-		for (int i = 0; i < amountOfSlaves - 1; i++)
-		{
-			temporaryArr[i].AssingValues(slaves[i].getName());
-		}
-		temporaryArr[amountOfSlaves - 1].AssingValues(slave.getName());
-		delete[]slaves;
-		slaves = nullptr;
-		slaves = new Student[amountOfSlaves];
-		slaves = temporaryArr;
-	}
-	void Deleter(int index) {
-		IncrementDecrease(1);
-		Student* temporaryArr = new Student[amountOfSlaves];
-		int tempIndex = 0;
-		for (int i = 0; i < amountOfSlaves+1; i++)
-		{
-			if (i + 1 == index) {
-				continue;
-			}
-			else {
-				temporaryArr[tempIndex].AssingValues(slaves[i].getName());
-				tempIndex++;
-			}
-		}
+	void Adder(Student slave) {										   //
+		IncrementIncrease(1);										   //
+		Student* temporaryArr = new Student[amountOfSlaves];		   //
+		for (int i = 0; i < amountOfSlaves - 1; i++)				   //
+		{															   //
+			temporaryArr[i].AssingValues(slaves[i].getName());		   //
+		}															   //Adder
+		temporaryArr[amountOfSlaves - 1].AssingValues(slave.getName());//
+		delete[]slaves;												   //
+		slaves = nullptr;											   //
+		slaves = new Student[amountOfSlaves];						   //
+		slaves = temporaryArr;										   //
+	}																   //
+	void Deleter(int index) {											  //
+		IncrementDecrease(1);											  //
+		Student* temporaryArr = new Student[amountOfSlaves];			  //
+		int tempIndex = 0;												  //
+		for (int i = 0; i < amountOfSlaves + 1; i++)					  //
+		{																  //
+			if (i + 1 == index) {										  //Deleter
+				continue;												  //
+			}															  //
+			else {														  //
+				temporaryArr[tempIndex].AssingValues(slaves[i].getName());//
+				tempIndex++;											  //
+			}															  //
+		}																  //
 		delete[] slaves;
 		slaves = temporaryArr;
 		slaves = new Student[amountOfSlaves];
@@ -68,21 +68,21 @@ public:
 		temporaryArr = nullptr;
 	}
 
-	void Renamer(int index, string name) {
-		slaves[index - 1].AssingValues(name);
-	}
+	void Renamer(int index, string name) {	 //
+		slaves[index - 1].AssingValues(name);//Renaming
+	}										 //
 	void Writer() {
-		for (int i = 0; i < amountOfSlaves; i++)
-		{
-			cout << "Name of student " << i + 1 << " is: " << slaves[i].getName() << endl;
-		}
-	}
-	static void IncrementIncrease(int x) {
-		amountOfSlaves += x;
-	}
-	static void IncrementDecrease(int x) {
-		amountOfSlaves -= x;
-	}
+		for (int i = 0; i < amountOfSlaves; i++)										  //
+		{																				  //
+			cout << "Name of student " << i + 1 << " is: " << slaves[i].getName() << endl;//Printer
+		}																				  //
+	}																					  //
+	static void IncrementIncrease(int x) {//
+		amountOfSlaves += x;			  //
+	}									  //Static methods
+	static void IncrementDecrease(int x) {//
+		amountOfSlaves -= x;			  //
+	}									  //
 };
 int Group::amountOfSlaves = 0;
 int main()
@@ -95,58 +95,58 @@ int main()
 		cin >> result;
 		cin.ignore();
 
-		if (result == 1) {
-			string name;
-			cout << "Write name: ";
-			cin >> name;
-			Student slave = Student(name);
-			obj.Adder(slave);
-			cout << endl;
-			cout << "Amount of students: " << Group::amountOfSlaves << endl << "List of students: " << endl;
-			obj.Writer();
+		if (result == 1) {																					//
+			string name;																					//
+			cout << "Write name: ";																			//
+			cin >> name;																					//
+			Student slave = Student(name);																	//Adding
+			obj.Adder(slave);																				//
+			cout << endl;																					//
+			cout << "Amount of students: " << Group::amountOfSlaves << endl << "List of students: " << endl;//
+			obj.Writer();																					//
+		}																									//
+
+
+		else if (result == 2) {											   //
+			if (Group::amountOfSlaves < 1) {							   //
+				cout << "Here is nobody to rename.\n";					   //
+			}															   //
+			else {														   //
+				int renamerIndex;										   //
+				cout << "White which slave you want to rename: ";		   //
+				cin >> renamerIndex;									   //
+				if (renamerIndex<1 || renamerIndex>Group::amountOfSlaves) {//Renaming
+					cout << "Invalid number.\n";						   //
+				}														   //
+				else {													   //
+					string name;										   //
+					cout << "Write name: ";								   //
+					cin >> name;										   //
+					obj.Renamer(renamerIndex, name);					   //
+					obj.Writer();										   //
+				}														   //
+			}
 		}
 
 
-		else if (result == 2) {
-			if (Group::amountOfSlaves < 1) {
-				cout << "Here is nobody to rename.\n";
-			}
-			else {
-				int renamerIndex;
-				cout << "White which slave you want to rename: ";
-				cin >> renamerIndex;
-				if (renamerIndex<1 || renamerIndex>Group::amountOfSlaves) {
-					cout << "Invalid number.\n";
-				}
-				else {
-					string name;
-					cout << "Write name: ";
-					cin >> name;
-					obj.Renamer(renamerIndex, name);
-					obj.Writer();
-				}
-			}
-		}
-
-
-		else if (result == 3) {
-			if (Group::amountOfSlaves<1) {
-				cout << "Here is nobody to fire.\n";
-			}
-			else {
-				int deleter;
-				cout << "White which slave you want to fire: ";
-				cin >> deleter;
-				if (deleter<1 || deleter>Group::amountOfSlaves) {
-					cout << "Invalid number.\n";
-				}
-				else {
-					obj.Deleter(deleter);
-					cout << endl;
-					cout << "Amount of students: " << Group::amountOfSlaves << endl << "List of students: " << endl;
-					obj.Writer();
-				}
-			}
+		else if (result == 3) {																						//
+			if (Group::amountOfSlaves < 1) {																		//
+				cout << "Here is nobody to fire.\n";																//
+			}																										//
+			else {																									//
+				int deleter;																						//
+				cout << "White which slave you want to fire: ";														//
+				cin >> deleter;																						//
+				if (deleter<1 || deleter>Group::amountOfSlaves) {													//Deleting
+					cout << "Invalid number.\n";																	//
+				}																									//
+				else {																								//
+					obj.Deleter(deleter);																			//
+					cout << endl;																					//
+					cout << "Amount of students: " << Group::amountOfSlaves << endl << "List of students: " << endl;//
+					obj.Writer();																					//
+				}																									//
+			}																										//
 		}
 	}
 }
